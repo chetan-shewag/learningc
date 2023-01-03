dbdcf20ee85065a01560d24bc60a94660495582e
chetan_shewag <chetanshewag213@gmail.com>
2023-01-03 10:50:48 +0530

    function topic

---
+++ FUNT.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+		   void sum();     //function decleration
+		   void main()
+{
+		   clrscr();
+		   sum();         //function calling
+		   getch();
+}
+		  void sum()        // function defination
+	     {
+		  int a,b,c;
+		  printf("\n enter two number");
+		  scanf("%d%d",&a,&b);
+
+		      c=a+b;
+		  printf("\n sum=%d",c);
+	      }
